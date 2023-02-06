67dcd44787d8f6d51ee7e0be5e8511ab64081f9a
Exequiel65 <marcos97britos@gmail.com>
2023-02-06 15:37:20 -0300

    home separated into components

---
+++ client/src/components/home/header/Index.jsx
@@ -0,0 +1,18 @@
+
+import React from 'react';
+import NavMenu from './NavMenu';
+import OptionsComunity from './OptionsComunity';
+import InfoComunity from './InfoComunity';
+
+const Index = () => {
+    return (
+        <>
+            <NavMenu />
+            <OptionsComunity />
+            <InfoComunity />
+        </>
+    );
+}
+
+
+export default Index;
