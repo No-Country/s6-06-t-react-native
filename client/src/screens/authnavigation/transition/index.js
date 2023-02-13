744520c5dc7c5ef07ef843ceee71f0cf0d89c8c8
Leandro Pereyra <me.leandrop@gmail.com>
2023-02-13 11:03:39 -0300

    fix advertencia ForwardRef

--- client/src/screens/authnavigation/transition/index.js
+++ client/src/screens/authnavigation/transition/index.js
@@ -1,34 +1,19 @@
-import {
-  View,
-  Text,
-  StyleSheet,
-  useWindowDimensions,
-  Image,
-  ActivityIndicator,
-} from "react-native";
-import { useEffect } from "react";
+import { View, Text, StyleSheet, Image } from "react-native";
 import { styles } from "./styles";
-import LottieView from "lottie-react-native";
 import Logo from "../../../../assets/Logo.png";
-import { useState } from "react";
-import AsyncStorage from "@react-native-async-storage/async-storage";
 import { useSelector } from "react-redux";
+import { useNavigation } from "@react-navigation/native";
 
-const Transition = ({ navigation }) => {
-  const { width } = useWindowDimensions();
-  const { height } = useWindowDimensions();
-  const { isLoading, token, error } = useSelector((state) => state.login);
-
+const Transition = () => {
+  const { isLoading, token } = useSelector((state) => state.login);
+  const navigation = useNavigation();
   if (isLoading) {
     return (
       <View style={[styles.container, StyleSheet.absoluteFillObject]}>
-        {/* <View style={{backgroundColor: 'white',opacity: .25, height: 22, width: width, position:'absolute', top: 0}}></View> */}
         <View style={styles.main}>
           <Image source={Logo} style={{ width: 350, height: 350 }} />
           <Text style={[styles.text]}>Iniciando sesión...</Text>
-          {/* {isLoading ? <ActivityIndicator size="large"/> : <></>} */}
         </View>
-        {/* <Text style={[styles.Text, {position: 'absolute', top: height/2+50, color: 'white'}]}>Cargando Mapas</Text> */}
       </View>
     );
   }
