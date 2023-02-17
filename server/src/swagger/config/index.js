7904bcea9854b7a1f4f313e89ffd0f41291efd4e
César Herrera <herrera.cesar.arg@outlook.com>
2023-02-17 00:42:21 -0300

    Working with Swagger

--- server/src/swagger/config/index.js
+++ server/src/swagger/config/index.js
@@ -1,25 +1,25 @@
-const swaggerJsDoc =require("swagger-jsdoc");
+const swaggerJsDoc = require('swagger-jsdoc');
 
-  const options = {
-      definition: {
-          openapi: '3.0.0',
-          info: {
-              title: 'Sin Fronteras API',
-              version: '1.0.0',
-              description: 'A Express and Mongo API'
-          },
-          servers: [
-              {
-                  url: process.env.URL
-              }
-          ]
-      },
-      apis: [
-          './src/swagger/routes/*.yaml',
-          './src/swagger/models/*.yaml',
-          './src/swagger/examples/*.yaml'
-      ]
-  };
+const options = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'Sin Fronteras API',
+            version: '1.0.0',
+            description: 'A Express and Mongo API'
+        },
+        servers: [
+            {
+                url: process.env.URL
+            }
+        ]
+    },
+    apis: [
+        './src/swagger/routes/*.yaml',
+        './src/swagger/models/*.yaml',
+        './src/swagger/examples/*.yaml'
+    ]
+};
 
 const specs = swaggerJsDoc(options);
-module.exports=specs
+module.exports = specs;
