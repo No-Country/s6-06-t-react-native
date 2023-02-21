a189b24305ab96793e4992e163eb6e67ba8d7e36
Leandro Pereyra <me.leandrop@gmail.com>
2023-02-21 02:13:29 -0300

    Input SobreMi - Perfil Profesional

--- client/src/screens/profile_navigation/professional_profile/componentes/SobreMi.jsx
+++ client/src/screens/profile_navigation/professional_profile/componentes/SobreMi.jsx
@@ -1,48 +1,162 @@
-import { Feather } from '@expo/vector-icons'
-import React from 'react'
-import { View, Text, StyleSheet, Modal } from 'react-native'
+import { Feather, AntDesign } from "@expo/vector-icons";
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Modal,
+  TouchableOpacity,
+  TextInput,
+  Dimensions,
+} from "react-native";
+import { colors } from "../../../../constants";
 
-const SobreMi = () => {
+const SobreMi = ({ data }) => {
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [isFocused, setIsFocused] = useState(false);
+  const [aboutMe, setAboutMe] = useState("");
   return (
     <View style={styles.container}>
-        <View style={{flexDirection: "row", justifyContent:"space-between", padding:2}}>
-        <Text style={{fontSize: 20, fontWeight: "bold", marginLeft: 10, marginTop: 10}}>Sobre mi</Text>
-        <Feather name="edit-3" size={22} color="blue" style={{marginRight: 10, marginTop: 10}}/>
-        </View>
-        <View style={styles.txtcontainer}>
-            <Text style={styles.text}>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            </Text>
-        </View>
-    </View>
-  )
-}
+      <TouchableOpacity
+        style={{
+          flexDirection: "row",
+          justifyContent: "space-between",
+          padding: 2,
+        }}
+        onPress={() => setIsModalVisible(true)}
+      >
+        <Text
+          style={{
+            fontSize: 20,
+            fontWeight: "bold",
+            marginLeft: 10,
+            marginTop: 10,
+          }}
+        >
+          Sobre mi
+        </Text>
+        <Feather
+          name="edit-3"
+          size={22}
+          color="blue"
+          style={{ marginRight: 10, marginTop: 10 }}
+        />
+      </TouchableOpacity>
+      <View style={styles.txtcontainer}>
+        <Text style={styles.text}>
+          {data
+            ? data.about
+            : "Lorem ipsum dolor sit amet, consectetur adipiscing elit."}
+        </Text>
+      </View>
 
-export default SobreMi
+      {/* -------------------MODAL--------------------- */}
+
+      <Modal visible={isModalVisible} animationType="slide">
+        <View style={styles.modalContainer}>
+          <TouchableOpacity onPress={() => setIsModalVisible(false)}>
+            <AntDesign name="close" size={25} style={styles.x} />
+          </TouchableOpacity>
+          <Text style={styles.sobreMi}>Sobre mi</Text>
+          <Text style={styles.filasReq}>*Filas requeridas</Text>
+          <Text style={styles.desc}>Descripción</Text>
+          <View
+            style={[
+              styles.inputContainer,
+              { borderColor: isFocused ? colors.primary : "transparent" },
+            ]}
+          >
+            <TextInput
+              value={aboutMe}
+              onChangeText={(value) => setAboutMe(value)}
+              placeholder={
+                data
+                  ? data.about
+                  : "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+              }
+              onFocus={() => setIsFocused(true)}
+              onBlur={() => setIsFocused(false)}
+              multiline={true}
+            />
+          </View>
+          <View style={styles.numContainer}>
+            <Text>{aboutMe.length ? aboutMe.length : 0}/3000</Text>
+          </View>
+        </View>
+      </Modal>
+    </View>
+  );
+};
+
+export default SobreMi;
 
 //Style component as a box with shadow and rounded corners height 132px
 
 const styles = StyleSheet.create({
-    container: {
-        backgroundColor: "white",
-        width: "100%",
-        minHeight: 132,
-        borderRadius: 10,
-        shadowColor: "#000",
-        shadowOffset: {
-            width: 0,
-            height: 2,
-        },
-        shadowOpacity: 0.25,
-        shadowRadius: 3.84,
-        elevation: 5,
+  container: {
+    backgroundColor: "white",
+    width: "100%",
+    minHeight: 132,
+    borderRadius: 10,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
     },
-    txtcontainer: {
-        flex: 1,
-        justifyContent: "center",
-        padding: 10,
-    },
-    text: {
-        color: "#888888",
-    }
-})
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+  txtcontainer: {
+    flex: 1,
+    justifyContent: "center",
+    padding: 10,
+  },
+  text: {
+    color: "#888888",
+  },
+  modalContainer: {
+    marginTop: 10,
+    marginHorizontal: 10,
+  },
+  inputContainer: {
+    fontSize: 14,
+    fontWeight: "500",
+    padding: 10,
+    borderRadius: 10,
+    borderWidth: 1,
+    width: Dimensions.get("window").width - 25,
+    height: 155,
+    backgroundColor: "#fff",
+    backgroundColor: colors.input_background,
+    marginVertical: 15,
+  },
+  sobreMi: {
+    fontSize: 20,
+    fontWeight: "600",
+  },
+  filasReq: {
+    fontSize: 12,
+    fontWeight: "400",
+    color: "#626A6D",
+    marginLeft: 5,
+  },
+  desc: {
+    fontSize: 15,
+    fontWeight: "400",
+    marginTop: 15,
+    marginBottom: -10,
+  },
+  x: {
+    color: "#4245E5",
+    fontSize: 25,
+    marginLeft: -2,
+    marginBottom: 5,
+  },
+  numContainer: {
+    display: "flex",
+    alignItems: "flex-end",
+    marginRight: 15,
+    marginTop: -10,
+  },
+});
