ab81cbcbef5636b838f8eccdfd0e0b1b2d5c1a50
katu <93katu@gmail.com>
2023-03-01 22:02:22 -0300

    foto redondeada

--- client/src/screens/profile_navigation/professional_profile/PerfilProfesional.jsx
+++ client/src/screens/profile_navigation/professional_profile/PerfilProfesional.jsx
@@ -40,10 +40,10 @@ const PerfilProfesional = () => {
       <TopBar tabname="Perfil profesional" />
       <ScrollView>
         <View style={styles.ppContainer}>
-          <View>
+          <View style={styles.imagenAvatar}>
             <Image
               source={{ uri: profileImg }}
-              style={{ width: 100, height: 100 }}
+              style={{ width: 100, height: 100, borderRadius: 100 }}
             />
           </View>
           <TouchableOpacity style={styles.editButton} onPress={() => setIsModalVisible(true)}>
